c262dfc18ca45ed6548d9c6475436023487bf269
Chris Dodd <cd105306@ohio.edu>
2021-01-29 17:40:20 -0500

    Lab 2 program commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Chris Dodd
+ *        Date: 1/29/2021
+ * Description: Lab 2 program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
